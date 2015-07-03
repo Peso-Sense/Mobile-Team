e968de3fa3b27e9c8a943ec695186de577fb2282
mobile 2 <moxultimate@gmail.com>
2015-07-03 13:15:34 +0800

    pesosSense

---
+++ repo/PesoSense/app/src/main/java/one/com/pesosense/activity/LoginMain.java
@@ -0,0 +1,19 @@
+package one.com.pesosense.activity;
+
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by mobile2 on 7/3/15.
+ */
+public class LoginMain extends FragmentActivity {
+
+
+
+
+
+
+
+
+
+
+}
