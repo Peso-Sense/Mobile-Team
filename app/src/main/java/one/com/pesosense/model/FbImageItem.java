1eba623157be64d1aed29dc3bad8c1d16f189a23
mnl-assasin <mykelneds.03@gmail.com>
2015-07-13 14:22:57 +0800

    MAINGGIT KA RJ

---
+++ app/src/main/java/one/com/pesosense/model/FbImageItem.java
@@ -0,0 +1,42 @@
+package one.com.pesosense.model;
+
+/**
+ * Created by mykelneds on 7/13/15.
+ */
+public class FbImageItem {
+
+    String id;
+    String message;
+    String link;
+    int likes;
+    int comment;
+
+    public FbImageItem(String id, String message, String link, int likes, int comment) {
+        this.id = id;
+        this.message = message;
+        this.link = link;
+        this.likes = likes;
+        this.comment = comment;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public int getLikes() {
+        return likes;
+
+    }
+
+    public int getComment() {
+        return comment;
+    }
+}
